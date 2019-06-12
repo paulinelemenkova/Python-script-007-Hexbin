cd00a9cec07dd01779c1fe4e5c637951932d2081
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 18:12:47 +0800

    Add files via upload

--- Script-007-Hexbin-KDE.py
+++ Script-007-Hexbin-KDE.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[53]:
-
-
+#
 import seaborn as sb
 from matplotlib import pyplot as plt
 import pandas as pd
 import os
 os.chdir('/Users/pauline/Documents/Python')
+#
 dfB = pd.read_csv("Tab-Bathy.csv")
 df = dfB.melt(id_vars=['observ'], 
               value_vars=['profile1', 'profile2', 'profile3', 'profile4', 
@@ -21,29 +19,9 @@ df = dfB.melt(id_vars=['observ'],
 #print(df.head)
 sb.set(style="white")
 sb.set_color_codes()
-#sb.jointplot(x='Profiles',y='Depths',data=df,kind='kde')
-#sb.pairplot(df,diag_kind="hex",kind="scatter",palette="husl")
-sb.jointplot(x='observ',y='Depths',data=df,kind='kde', dropna=True, color="m")
+#
+sb.jointplot(x='observ',y='Depths',data=df, kind='kde', dropna=True, color="m")
 sb.jointplot("observ", "Depths", data=df, kind="hex", dropna=True)
 #plt.title('Mariana Trench', fontsize=10, fontfamily='serif')
 plt.subplots_adjust(bottom=0.15,top=0.85, right=0.90, left=0.10)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
