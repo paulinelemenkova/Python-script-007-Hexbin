1648b40be1678c99d9f9e1bbc0f609cf4b83d3b5
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 10:04:41 +0800

    Add files via upload

--- Script-007-Hexbin-KDE.py
+++ Script-007-Hexbin-KDE.py
@@ -7,21 +7,20 @@ import pandas as pd
 import os
 os.chdir('/Users/pauline/Documents/Python')
 #
+profiles_nrs = list(map(lambda x: x, range(25)))
+profiles_list = []
+for i in profiles_nrs:
+    profiles_list.append('profile{}'.format(i + 1))
+#
 dfB = pd.read_csv("Tab-Bathy.csv")
-df = dfB.melt(id_vars=['observ'], 
-              value_vars=['profile1', 'profile2', 'profile3', 'profile4', 
-                          'profile5', 'profile6', 'profile7', 'profile8',
-                         'profile9', 'profile10', 'profile11', 'profile12',
-                         'profile13', 'profile14', 'profile15', 'profile16', 
-                          'profile17', 'profile18', 'profile19', 'profile20',
-                         'profile21', 'profile22', 'profile23', 'profile24','profile25'],
-              var_name='Profiles', value_name='Depths')
+df = dfB.melt(id_vars = ['observ'],
+              value_vars = profiles_list,
+              var_name = 'Profiles', value_name = 'Depths')
 #print(df.head)
-sb.set(style="white")
+sb.set(style = "white")
 sb.set_color_codes()
 #
-sb.jointplot(x='observ',y='Depths',data=df, kind='kde', dropna=True, color="m")
-sb.jointplot("observ", "Depths", data=df, kind="hex", dropna=True)
-#plt.title('Mariana Trench', fontsize=10, fontfamily='serif')
-plt.subplots_adjust(bottom=0.15,top=0.85, right=0.90, left=0.10)
+sb.jointplot(x = 'observ',y = 'Depths',data = df, kind='kde', dropna = True, color = "m")
+sb.jointplot("observ", "Depths", data = df, kind = "hex", dropna = True)
+plt.subplots_adjust(bottom = 0.15,top = 0.85, right = 0.90, left = 0.10)
 plt.show()
